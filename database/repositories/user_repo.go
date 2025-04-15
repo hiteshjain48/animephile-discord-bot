365d906f0dd7411403089e9cd48300c3a18672fa
Hitesh Jain <74195956+hiteshjain48@users.noreply.github.com>
2025-04-15 10:27:37 +0000

    column name fixes

--- database/repositories/user_repo.go
+++ database/repositories/user_repo.go
@@ -19,13 +19,13 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 
 func (ur *UserRepository) Create(user models.User) error {
 	return ur.repo.Execute(
-		"INSERT INTO users (discord_id, user_name, joined_at) VALUES ($1, $2, $3) ON CONFLICT (discord_id) DO NOTHING", user.DiscordID, user.UserName, user.JoinedAt,
+		"INSERT INTO users (discord_id, username, joined_at) VALUES ($1, $2, $3) ON CONFLICT (discord_id) DO NOTHING", user.DiscordID, user.UserName, user.JoinedAt,
 	)
 }
 
 func (ur *UserRepository) GetByID(id string) (models.User, error) {
 	var user models.User
-	row := ur.repo.QueryRow("SELECT discord_id, user_name FROM users WHERE discord_id = $1", id)
+	row := ur.repo.QueryRow("SELECT discord_id, username FROM users WHERE discord_id = $1", id)
 
 	err := row.Scan(&user.DiscordID, &user.UserName)
 	if err != nil {
@@ -42,7 +42,7 @@ func (ur *UserRepository) List() ([]models.User, error) {
 	var users []models.User
 
 	err := ur.repo.Query(
-		"SELECT discord_id, user_name from users",
+		"SELECT discord_id, username from users",
 		func(rows *sql.Rows) error {
 			for rows.Next() {
 				var user models.User
